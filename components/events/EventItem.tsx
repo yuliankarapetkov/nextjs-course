d06d9b559a7a358a27df4fafcb518d169ab790b5
Yulian Karapetkov <yulian.karapetkov@gmail.com>
2023-05-23 11:20:55 +0300

    feat: structure event item

--- components/events/EventItem.tsx
+++ components/events/EventItem.tsx
@@ -1,9 +1,42 @@
+import Link from "next/link";
+
 interface Props {
     event: any;
 }
 
 export default function EventItem({ event }: Props) {
+    const { title, image, date, location, id } = event;
+
+    const formattedDate = new Date(date).toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "long",
+        year: "numeric",
+    });
+
+    const formattedAddress = location.replace(", ", "\n");
+    const link = `/events/${id}`
+
     return (
-        <li key={event.id}>{event.name}</li>
+        <li>
+            <img src={`/${image}`} alt={title} />
+
+            <div>
+                <div>
+                    <h2>{title}</h2>
+
+                    <div>
+                        <time>{formattedDate}</time>
+                    </div>
+
+                    <div>
+                        <address>{formattedAddress}</address>
+                    </div>
+                </div>
+
+                <div>
+                    <Link href={link}>Explore Event</Link>
+                </div>
+            </div>
+        </li>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import EventList from "@/components/events/EventList"
+import { getFeaturedEvents } from "@/dummy-data"
 
 export default function Home() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <>
       <div>
         <h1>Home Page</h1>
+
+        <EventList events={featuredEvents} />
       </div>
     </>
   )
